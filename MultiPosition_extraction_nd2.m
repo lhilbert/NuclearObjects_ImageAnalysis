f5aec645697d0f570c1936cbd63e3704454c1228
Lennart Hilbert <lennart.hilbert@gmail.com>
2025-09-23 12:30:50 +0200

    Add back reader script
    Old reader script added, still without lazy reader being used in it. This is contained in the reader script with the lazy file name extension.

---
+++ MultiPosition_extraction_nd2.m
@@ -0,0 +1,135 @@
+clear all
+
+plotFlag = true;
+
+sourceDirectories = { ...
+	'./ImageData/Ctrl/',...
+	'./ImageData/FP/',...
+	};
+extractTargetFolder = 'ExtractedStacks';
+condInds = (1:2)';
+condLabels = {...
+	'Ctrl','FP'
+	};
+maxNumSeries = Inf;
+
+skipList = []; % Directories to skip, for example if already done
+% Leave empty array [] if all directories shoudl be processed.
+
+useChannel_inds = [3,2,1]; %Ser5P, Ser2P, DNA
+numChannels = numel(useChannel_inds);
+scaleChannels = {[-Inf,Inf],[-Inf,Inf],[-Inf,Inf]};
+
+numDirs = numel(sourceDirectories);
+
+for cc = 1:numDirs
+    
+	fprintf('Extracting image data from directory %d of %d\n',cc,numDirs)
+	
+	if ismember(cc,skipList)
+		
+		fprintf('On skip list, skipping to next directory.\n')
+		
+	else
+		
+		thisDir = sourceDirectories{cc};
+		
+		listing = rdir([thisDir,'*.nd2'],'~contains(name,''._'')');
+		
+		numFiles = numel(listing);
+		
+		imgCell = cell(numFiles,1);
+		imgSizeCell = cell(numFiles,1);
+		pixelSizeVec = cell(numFiles,1);
+		zzStepVec = cell(numFiles,1);
+		
+		stackCounter = 0;
+		mkdir(sprintf('./%s/Cond_%d',extractTargetFolder,cc))
+		
+		for ff = 1:numFiles
+			
+			fprintf('Extracting images from file %d of %d\n',ff,numFiles)
+			
+			combined_filepath = ...
+				fullfile(listing(ff).name);
+			
+			[imgCell,imgSizeCell,...
+				this_voxelSize_cell,seriesName_cell] = ...
+				nd2read(combined_filepath,maxNumSeries);
+			
+			pixelSizeVec = cellfun(@(elmt)elmt(1),this_voxelSize_cell);
+			zzStepVec = cellfun(@(elmt)elmt(3),this_voxelSize_cell);
+			
+			numImgs = numel(imgCell);
+			for kk = 1:numImgs
+				imgCell{kk} = imgCell{kk}(useChannel_inds);
+			end
+			
+			if plotFlag
+				
+				for kk = 1:numImgs
+					
+					thisPixelSize = pixelSizeVec(kk);
+					thisSize = imgSizeCell{kk};
+					
+					numChannels = numel(imgCell{kk});
+					
+					for cc_2 = 1:numChannels
+						
+						subplot(1,numChannels,cc_2)
+						
+						imagesc([0,thisSize(2)].*thisPixelSize,...
+							[0,thisSize(1)].*thisPixelSize,...
+							imgCell{kk}{cc_2}(:,:,ceil(thisSize(3)./2)),...
+							scaleChannels{1})
+						axis equal tight
+						
+						xlabel('x [\mum]')
+						
+						colormap((gray))
+						
+					end
+					
+					disp(sprintf( ...
+						'Directory number %d, Condition %s, File %d/%d, Image %d/%d.', ...
+						cc,condLabels{condInds(cc)},ff,numFiles,kk,numImgs))
+					
+					waitforbuttonpress
+					
+				end
+				
+			end
+			
+			% Apply exclusion indices
+			keepInds = 1:numImgs;
+			imgCell = imgCell(keepInds);
+			imgSizeCell = imgSizeCell(keepInds);
+			pixelSizeVec = pixelSizeVec(keepInds);
+			zzStepVec = zzStepVec(keepInds);
+			
+			% --- saving of the retained images
+			
+			condName = condLabels{cc};
+			condInd = condInds(cc);
+			
+			% Save all images into a separate file, but contained in a folder that
+			% contains all images for this condition
+			
+			numImgs = numel(imgCell);
+			for kk = 1:numImgs
+				stackCounter = stackCounter+1;
+				imgStack = imgCell{kk};
+				imgSize = imgSizeCell{kk};
+				pixelSize = pixelSizeVec(kk);
+				zStepSize = zzStepVec(kk);
+				save(sprintf('./%s/Cond_%d/Image_%d.mat',...
+					extractTargetFolder,cc,stackCounter),...
+					'imgStack','imgSize','pixelSize','zStepSize',...
+					'condInd','condName')
+			end
+			
+		end
+		
+	end
+	
+end
---
+++ MultiPosition_extraction_nd2_lazy.m
@@ -0,0 +1,111 @@
+clear all
+
+plotFlag = true;
+
+sourceDirectories = { ...
+	'./ImageData/Folder_2/',...
+	'./ImageData/Folder_1/',...
+	};
+extractTargetFolder = './ExtractedStacks';
+condInds = (1:2)';
+condLabels = {...
+	'Example 2',...
+    'Example 1',...
+	};
+maxNumSeries = 3;
+
+skipList = []; % Directories to skip, for example if already done
+% Leave empty array [] if all directories should be processed.
+useChannel_inds = [7,5,4,2]; % Hoechst, Alexa 488, Alexa 594, STAR Red
+
+numChannels = numel(useChannel_inds);
+scaleChannels = {[-Inf,Inf],[-Inf,Inf],[-Inf,Inf],[-Inf,Inf]};
+
+numDirs = numel(sourceDirectories);
+
+for cc = 1:numDirs
+    
+	fprintf('Extracting image data from directory %d of %d\n',cc,numDirs)
+	
+	if ismember(cc,skipList)
+		
+		fprintf('On skip list, skipping to next directory.\n')
+		
+	else
+		
+		thisDir = sourceDirectories{cc};
+		
+		listing = rdir([thisDir,'*.nd2'],'~contains(name,''._'')');
+		
+		numFiles = numel(listing);
+
+        condName = condLabels{cc};
+    	condInd = condInds(cc);
+        
+		stackCounter = 0;
+		mkdir(sprintf('./%s/Cond_%d',extractTargetFolder,cc))
+		
+		for ff = 1:numFiles
+			
+			fprintf('Extracting images from file %d of %d\n',ff,numFiles)
+			
+			combined_filepath = ...
+				fullfile(listing(ff).name);
+
+            reader = OMEImageReaderLazy(combined_filepath);
+            nSeries = reader.getNumSeries(); % Fast, uses metadata only
+            numSeries = min(nSeries,maxNumSeries);
+            
+            for ss = 1:numSeries
+                
+                % read in the series
+                imgStack = reader.getChannelStack(ss,useChannel_inds); % Loads channel 1 and 3 images only, on demand
+                fprintf('Image series %d of %d extracted\n',ss,numSeries)
+
+                % save the series to an .mat file
+                stackCounter = stackCounter+1;
+			 	pixelSize = reader.getPixelSizeXY(ss);
+                pixelSize = pixelSize(1);
+				zStepSize = reader.getZStepSize(ss);
+                imgSize = reader.getStackSizeXYZ(ss);
+				save(sprintf('./%s/Cond_%d/Image_%d.mat',...
+					extractTargetFolder,cc,stackCounter),...
+					'imgStack','imgSize','pixelSize','zStepSize',...
+					'condInd','condName','-v7.3')
+
+                if plotFlag
+
+                    numChannels = numel(imgStack);
+
+                    for cc_2 = 1:numChannels
+
+                        subplot(1,numChannels,cc_2)
+
+                        imagesc([0,imgSize(1)].*pixelSize,...
+                            [0,imgSize(2)].*pixelSize,...
+                            imgStack{cc_2}(:,:,ceil(imgSize(3)./2)),...
+                            prctile(imgStack{cc_2}(:),[0.1,99.9]))
+                        axis equal tight
+
+                        xlabel('x [\mum]')
+
+                        colormap((gray))
+
+                    end
+
+                    disp(sprintf( ...
+                        'Directory number %d, Condition %s, File %d/%d, Image %d/%d.', ...
+                        cc,condLabels{condInds(cc)},ff,numFiles,ss,numSeries))
+
+                    waitforbuttonpress
+
+                end
+
+            end
+            reader.close(); % Always close reader after use!
+
+		end
+		
+	end
+	
+end
